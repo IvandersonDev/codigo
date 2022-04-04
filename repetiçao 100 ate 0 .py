2784462202b933629de5ddb3d32f8621efd2baf2
Ivanderson do Amaral Tavares <102988133+ivandersonpy@users.noreply.github.com>
2022-04-04 15:01:48 -0300

    Add files via upload

---
+++ repetiçao 100 ate 0 .py
@@ -0,0 +1,17 @@
+#conte de 100 ate 0
+
+numero= 100
+while numero >=0:
+    print(numero)
+    numero-=1
+
+print('Programa finalizado')
+
+
+
+
+
+
+
+
+
---
+++ soma aula 31.03.2022.py
@@ -0,0 +1,22 @@
+contador = 0
+soma = 0
+while contador < 7:
+    contador = contador + 1
+    #contador+=1
+    num = int(input('Digite um numero:'))
+    soma = soma + num
+
+print(soma)
+
+
+
+
+
+
+
+
+
+
+
+
+
