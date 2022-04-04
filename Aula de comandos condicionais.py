6fdc45b87e9e35f56f974d7b0d85a88eb4660594
Ivanderson do Amaral Tavares <102988133+ivandersonpy@users.noreply.github.com>
2022-04-04 14:35:48 -0300

    Add files via upload

---
+++ Aula de comandos condicionais.py
@@ -0,0 +1,9 @@
+#Aula de comandos condicionais
+
+num = int(input('digite um número inteiro:'))
+
+if(num%2 == 0):
+    print("Esse número" ,num, "é par")
+
+else:
+    print("Esse número" ,num,"é impar" )
